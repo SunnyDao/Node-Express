24862bd8cc73067a0f06ef475a16ca94e997bfff
zengxiangzhi <zengxiangzhi@zhongan.io>
2017-08-02 20:27:14 +0800

    添加一个中间件

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,35 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+	res.render('index', { title: 'Express' });
 });
+
+router.post('/login', (req, res, next) => {
+	(async () => {
+		const user = await User.login(req.body.phoneNumber, req.body.password)
+
+		const token = JWT.sign({
+			_id: user._id,
+			iat: Date.now(),
+			expire: Date.now() + 24 * 60 * 60 * 1000
+		}, JWT_SECRET)
+
+		return {
+			code: 0,
+			data: {
+				user: user,
+				token: token,
+			}
+		}
+	})()
+		.then(r => {
+			res.json(r)
+		})
+		.catch(e => {
+			next(e)
+		})
+})
+
 
 module.exports = router;
